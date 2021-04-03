d0c5a9fa847ac426bd8a14f3f295ca1d2182d098
Wennim <wennim1234@gmail.com>
2021-04-02 22:58:44 -0700

    final update

--- FFT.py
+++ FFT.py
@@ -3,7 +3,7 @@ import numpy as np
 import serial
 import time
 
-Fs = 128.0;  # sampling rate
+Fs = 93.0;  # sampling rate
 Ts = 1.0/Fs; # sampling interval
 t = np.arange(0,1,Ts) # time vector; create Fs samples between 0 and 1.0 sec.
 y = np.arange(0,1,Ts) # signal vector; create Fs samples
@@ -27,7 +27,7 @@ Y = np.fft.fft(y)/n*2 # fft computing and normalization
 Y = Y[range(int(n/2))] # remove the conjugate frequency parts
 
 fig, ax = plt.subplots(2, 1)
-ax[0].plot(t,y)
+ax[0].plot(t,y*3.1)
 ax[0].set_xlabel('Time')
 ax[0].set_ylabel('Amplitude')
 ax[1].plot(frq,abs(Y),'r') # plotting the spectrum
